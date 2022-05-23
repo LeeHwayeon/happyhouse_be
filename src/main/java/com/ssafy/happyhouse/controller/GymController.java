085bc6a2dc8082bd8a9efb1ea42309b10421eec8
Hwayeon <snw4002@hanmail.net>
2022-05-23 16:22:21 +0900

    add : gym

--- src/main/java/com/ssafy/happyhouse/controller/GymController.java
+++ src/main/java/com/ssafy/happyhouse/controller/GymController.java
@@ -7,25 +7,22 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ssafy.happyhouse.model.service.GymService;
 
-
 @RestController
 @RequestMapping("/gym")
 @CrossOrigin(origins = { "*" }, maxAge = 6000)
 public class GymController {
-	
+
 	private static final String SUCCESS = "success";
 	private static final String FAIL = "fail";
 
 	@Autowired
 	private GymService service;
-	
-	
+
 	@GetMapping()
 	public ResponseEntity<?> parklist() {
 		try {
@@ -33,6 +30,6 @@ public class GymController {
 		} catch (Exception e) {
 			e.printStackTrace();
 			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
-		} 
+		}
 	}
 }
